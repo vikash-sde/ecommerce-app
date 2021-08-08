8d289819893d869c3d4404f4ed9d2df5d32ce123
Vikash Gupta <vikashg2512@gmail.com>
2021-08-08 12:51:33 +0530

    first realease

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./container/ProductListing";
+import Header from "./container/Header";
+import "./App.css";
+import ProductDetail from "./container/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <ProductListing />
+          </Route>
+          <Route path="/product/:productId">
+            <ProductDetail />
+          </Route>
+          <Route>404 Not found !</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/container/ProductListing.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+import axios from "axios";
+import { setProducts } from "../redux/actions/productAction";
+
+const ProductListing = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log("Products :", products);
+
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductListing;
